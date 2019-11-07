0a1b2a7e05a165e49595d8535c91210a76853785
Janek Lauer <janek@lauer.email>
2019-11-07 12:28:53 +0100

    rename to main

--- ansible.sh
+++ ansible.sh
@@ -3,4 +3,4 @@ if (( $EUID != 0 )); then
     echo "Must be run as root"
     exit
 fi
-ansible-playbook local.yml --connection=localhost -i hosts.ini
+ansible-playbook main.yml --connection=localhost -i hosts.ini
